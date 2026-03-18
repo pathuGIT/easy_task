cc1e8fe35173394cfb9a277fd62c3b04296988fb
pathuGIT <wannigelakshan@gmail.com>
2026-03-19 00:15:33 +0530

    Create RegisterResponse.java

---
+++ backend/src/main/java/com/easytask/backend/dto/RegisterResponse.java
@@ -0,0 +1,27 @@
+package com.easytask.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+    private Long id;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
