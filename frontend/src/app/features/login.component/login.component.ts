79938d6f2cdf40e541d8842980712bf36025ba83
pathuGIT <wannigelakshan@gmail.com>
2026-03-19 21:51:10 +0530

    user login component

--- frontend/src/app/features/login.component/login.component.ts
+++ frontend/src/app/features/login.component/login.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from '../../services/auth-service';
+import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login.component',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  private router = inject(Router)
+  constructor(private authService : AuthService){}
 
+  username: string = '';
+  password: string = '';
+
+  login(){
+    this.authService.login(this.username, this.password).subscribe({
+      next: (req)=>{
+        console.log("msg: "+ req.message);
+        this.router.navigate(['/dashboard']); 
+      },
+      error: (err)=>{
+        console.log("Login err: "+ err)
+      }
+    })
+  }
 }
