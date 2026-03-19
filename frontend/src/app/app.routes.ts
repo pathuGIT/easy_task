545b73d0ab41ba8265bfec97330cea7826cd1ae3
pathuGIT <wannigelakshan@gmail.com>
2026-03-19 21:50:12 +0530

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,18 @@ import { LoginComponent } from './features/login.component/login.component';
 import { Home } from './features/home/home';
 import { RegisterComponent } from './features/register.component/register.component';
 import { AuthGuard } from './guards/auth.guard';
+import { DashboardComponent } from './features/dashboard.component/dashboard.component';
+import { TaskComponent } from './features/task.component/task.component';
+import { NewtaskComponent } from './features/newtask.component/newtask.component';
+import { UpadtetaskComponent } from './features/upadtetask.component/upadtetask.component';
 
 export const routes: Routes = [
-    {path:'', component: Home, canActivate: [AuthGuard]},
+    {path:'', component: Home},
     {path:'login', component:LoginComponent},
     {path:'register', component:RegisterComponent},
+    {path:'dashboard', component:DashboardComponent, canActivate: [AuthGuard]},
+    {path:'task', component:TaskComponent, canActivate: [AuthGuard]},
+    {path:'newtask', component:NewtaskComponent, canActivate: [AuthGuard]},
+    {path:'updatetask/:id', component:UpadtetaskComponent, canActivate: [AuthGuard]},
     {path: '**', redirectTo: '' }// fallback{}
 ];
