565dc84e71e81b58ba46d3a383e7882e17996f50
pathuGIT <wannigelakshan@gmail.com>
2026-03-19 21:51:33 +0530

    new task component

---
+++ frontend/src/app/features/newtask.component/newtask.component.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { TaskService } from '../../services/task-service';
+import { FormsModule } from '@angular/forms';
+import { TaskRequest } from '../../interfaces/taskReqest';
+
+@Component({
+  selector: 'app-newtask.component',
+  imports: [FormsModule],
+  templateUrl: './newtask.component.html',
+})
+export class NewtaskComponent {
+  private router = inject(Router);
+  private taskService = inject(TaskService);
+  private cd = inject(ChangeDetectorRef);
+
+  title: string = '';
+  description: string = '';
+  status: string = 'TO_DO';
+  warning: string = '';
+
+  createTask() {
+    const task: TaskRequest = {
+      title: this.title,
+      description: this.description,
+      status: this.status
+    };
+
+    this.taskService.addTask(task).subscribe({
+      next: (res) => {
+        console.log("Create task success:", res);
+        this.router.navigate(['/task']);
+      },
+      error: (err) => {
+        console.log("Create task err:", err.error?.message);
+        this.warning = err.error?.message;
+        this.cd.detectChanges();
+
+        setTimeout(() => {
+          this.warning = '';
+          this.cd.detectChanges();
+        }, 3000);
+      }
+    });
+  }
+}
