3b0d2cec6126336d02c611cea3fde8d216dca730
pathuGIT <wannigelakshan@gmail.com>
2026-03-19 21:49:03 +0530

    load user details on dashboard

---
+++ frontend/src/app/features/dashboard.component/dashboard.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+
+@Component({
+  selector: 'app-dashboard.component',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent implements OnInit{
+  private auth = inject(AuthService);
+  private cd = inject(ChangeDetectorRef);
+  
+  name: string = '';
+  id: string = '';
+
+  ngOnInit(){
+    this.auth.profile().subscribe({
+      next: (res)=>{
+        this.name = res.data.username;
+        this.id = res.data.id;
+        this.cd.detectChanges();
+      },
+      error: (err)=>{
+        console.log("err: "+ err.error?.message);
+      }
+    })
+  }
+
+
+}
